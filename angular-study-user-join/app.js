95f29b1a36ab9855641b1f29cf06f181203b62ff
u0lee22 <u0lee22@gmail.com>
2017-09-14 10:36:02 +0900

    20170913_edit

--- angular-study-user-join/app.js
+++ angular-study-user-join/app.js
@@ -9,5 +9,21 @@
 
     function ctrl($scope) {
         $scope.test = "hihihi";
-    }
+        $scope.userItem =
+        {
+            id : '',
+            pw : '',
+            name : '',
+            year: '',
+            month : '',
+            day : ''
+        };
+        $scope.userList = [];
+        $scope.createUser = function() {
+            $scope.userList.push(angular.copy($scope.userItem));
+            console.log($scope.userList);
+            console.log($scope.userItem);
+        };
+
+0    }
 })();
